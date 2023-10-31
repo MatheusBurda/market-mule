0859f466c1cab54b8a6e01403facd448305b9e63
MatheusBurda <matheusburda@alunos.utfpr.edu.br>
2023-10-31 20:25:07 -0300

    created web app

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import BasketList from './BasketList'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <BasketList />
+      </div>
+    </>
+  )
+}
+
+export default App
