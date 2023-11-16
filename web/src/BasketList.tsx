7ece495e422fb4ef05942654210cfd4453bfc134
MatheusBurda <matheusburda@alunos.utfpr.edu.br>
2023-11-15 23:06:07 -0300

    bugfix: fixed weight display

--- web/src/BasketList.tsx
+++ web/src/BasketList.tsx
@@ -24,7 +24,7 @@ export default function BasketList(props: { products: Item[] }) {
           <div className="about">
             <h1>{item.name.charAt(0).toUpperCase() + item.name.slice(1)}</h1>
             <p className='description'>Quantidade: {item.quantity}</p>
-            <p>Peso total: {item.total_weight}g</p>
+            <p>Peso total: {item.total_weight.toFixed(0)}g</p>
           </div>
           <div className="amount">
             <h3 className='price'>
@@ -39,7 +39,7 @@ export default function BasketList(props: { products: Item[] }) {
       <div className="total">
         <div>
           <h2>Sub-Total</h2>
-          <p className='description'>{props.products.reduce((a, b) => a + b.quantity, 0).toFixed()} itens</p>
+          <p className='description'>{props.products.reduce((a, b) => a + b.quantity, 0).toFixed(0)} itens</p>
         </div>
         <h1 className='totalAmount'>R$ {props.products.reduce((a, b) => a + b.total_price, 0).toFixed(2)}</h1>
       </div>
